a83709d615d58a311647fff1bda695e5f1b310f7
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-02 14:52:40 +0100

    Add route for rendering login form

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,3 +18,11 @@ router.get("/signup", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get("/login", async (req, res) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
