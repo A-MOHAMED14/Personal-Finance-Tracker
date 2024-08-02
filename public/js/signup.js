9641fedb3b5569b45b99c240da6f58b60f5db167
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-02 14:43:08 +0100

    Remove getStared click handler function

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,25 +1,11 @@
-const getStarted = async () => {
-  const response = await fetch("/signup");
-
-  if (response.ok) {
-    document.location.assign("/signup");
-  } else {
-    alert(response.statusText);
-  }
-};
-
-document
-  .querySelector("#get-started-btn")
-  .addEventListener("click", getStarted);
-
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector("#signup-name").value.trim();
   const email = document.querySelector("#signup-email").value.trim();
-  const password = document.querySelector("#signup-name").value.trim();
+  const password = document.querySelector("#signup-password").value.trim();
 
-  if ((name, email, password)) {
+  if (name && email && password) {
     const response = await fetch("/api/users/signup", {
       method: "POST",
       body: JSON.stringify({ name, email, password }),
@@ -35,5 +21,5 @@ const signupFormHandler = async (event) => {
 };
 
 document
-  .querySelector("#signup-btn")
+  .querySelector("#signup-form")
   .addEventListener("submit", signupFormHandler);
