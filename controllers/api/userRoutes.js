7a2879f8d8928881190ffbca821360e8b141c621
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-02 15:16:57 +0100

    implement login route with user authentication and session management

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,19 +2,54 @@ const router = require("express").Router();
 const { User, Income, Expense } = require("../../models/index.js");
 
 router.post("/signup", async (req, res) => {
-  const userData = await User.create(req.body);
+  try {
+    const userData = await User.create(req.body);
 
-  if (!userData) {
-    res.status(400).json({ message: "Unable to create new user record" });
-    return;
+    if (!userData) {
+      res.status(400).json({ message: "Unable to create new user record" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json({ message: "New user record created successfully" });
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
+});
 
-  req.session.save(() => {
-    req.session.user_id = userData.id;
-    req.session.logged_in = true;
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
-    res.status(200).json({ message: "New user record created successfully" });
-  });
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
