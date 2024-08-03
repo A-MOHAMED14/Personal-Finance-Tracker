476e824c3c02ddbea2654fd567f303db9339b265
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-03 15:23:50 +0100

    Add functionality to handle expense deletion with button click

--- public/js/updateExpense.js
+++ public/js/updateExpense.js
@@ -22,6 +22,26 @@ const handleExpenseUpdate = async (event) => {
   }
 };
 
+const handleExpenseDeletion = async () => {
+  const expenseId = new URLSearchParams(window.location.search).get("id");
+
+  if (expenseId) {
+    const response = await fetch(`/api/users/delete/expense?id=${expenseId}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
 document
   .querySelector("#update-expense-form")
   .addEventListener("submit", handleExpenseUpdate);
+
+document
+  .querySelector("#delete-expense-btn")
+  .addEventListener("click", handleExpenseDeletion);
