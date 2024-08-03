19436dfec32bf0ca36d2cca0d46741a3d2b5dc1f
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-03 15:18:42 +0100

    add route handler to update expense records by ID

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -54,6 +54,25 @@ router.put("/update/income", async (req, res) => {
   }
 });
 
+router.put("/update/expense", async (req, res) => {
+  try {
+    const expenseData = await Expense.update(req.body, {
+      where: {
+        id: req.query.id,
+      },
+    });
+
+    if (!expenseData) {
+      res.status(400).json({ message: "Unable to update expense record" });
+      return;
+    }
+
+    res.status(200).json({ message: "Income record updated successfully" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post("/signup", async (req, res) => {
   try {
     const userData = await User.create(req.body);
