8cf0f065f280c88e17e167e31ec335ae2a1c2d45
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-05 11:29:32 +0100

    Extract and format income amounts and dates for bar chart

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,3 +1,13 @@
 const incomesData = JSON.parse(
   document.querySelector("#incomes-data").textContent
 );
+
+let incomeAmounts = [];
+let incomeDates = [];
+
+incomesData.forEach((income) => {
+  incomeAmounts.push(income.amount);
+
+  const formatDate = new Date(income.date);
+  incomeDates.push(formatDate.toLocaleDateString());
+});
