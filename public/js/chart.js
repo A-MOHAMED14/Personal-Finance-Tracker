c967eca791cfa6c1f03854f0903ae13e6d28d12c
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-05 11:32:39 +0100

    Create bar chart for income amounts and dates using Chart.js

--- public/js/chart.js
+++ public/js/chart.js
@@ -11,3 +11,55 @@ incomesData.forEach((income) => {
   const formatDate = new Date(income.date);
   incomeDates.push(formatDate.toLocaleDateString());
 });
+
+const incomeChart = document.querySelector("#incomeChart").getContext("2d");
+
+Chart.defaults.font.family = "Lato";
+Chart.defaults.font.size = 18;
+Chart.defaults.color = "#777";
+
+const incomeBarChart = new Chart(incomeChart, {
+  type: "bar",
+  data: {
+    labels: incomeDates,
+    datasets: [
+      {
+        label: "Income Amounts",
+        data: incomeAmounts,
+        backgroundColor: "lightgreen",
+        borderWidth: 1,
+        borderColor: "#777",
+        hoverBorderWidth: 3,
+        hoverBorderColor: "#000",
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      title: {
+        display: true,
+        text: "Income",
+        font: {
+          size: 30,
+        },
+      },
+      legend: {
+        display: false,
+        position: "right",
+        labels: {
+          color: "#000",
+        },
+      },
+      tooltip: {
+        enabled: true,
+      },
+    },
+    layout: {
+      padding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+      },
+    },
+  },
+});
