d7ad2f3e1b9dd6d3739f59f3e70f109570408192
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-07 10:43:00 +0100

    Define model associations: User to Budget, Budget to Expense

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require("./user.js");
 const Income = require("./income.js");
 const Expense = require("./expense.js");
+const Budget = require("./budget.js");
 
 User.hasMany(Income, {
   foreignKey: "user_id",
@@ -20,4 +21,22 @@ Expense.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, Income, Expense };
+User.hasMany(Budget, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Budget.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Expense.hasOne(Budget, {
+  foreignKey: "expense_id",
+  onDelete: "CASCADE",
+});
+
+Budget.belongsTo(Expense, {
+  foreignKey: "expense_id",
+});
+
+module.exports = { User, Income, Expense, Budget };
