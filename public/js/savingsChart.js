f25f67274bbe707d7f2d0f865a97de10f642e954
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-08 17:08:09 +0100

    Add doughnut chart to display budget vs. expenses

--- public/js/savingsChart.js
+++ public/js/savingsChart.js
@@ -11,3 +11,50 @@ const totalBudget = JSON.parse(
 );
 
 const remainingBudget = totalBudget - totalExpense;
+
+const budgetChart = document.querySelector("#budgetChart").getContext("2d");
+
+Chart.defaults.font.family = "Lato";
+Chart.defaults.font.size = 18;
+Chart.defaults.color = "#777";
+
+const budgetBarChart = new Chart(budgetChart, {
+  type: "doughnut",
+  data: {
+    labels: ["Amount Spent", "Remaining Budget"],
+    datasets: [
+      {
+        label: "",
+        data: [totalExpense, remainingBudget],
+        backgroundColor: ["#d40000", "#4c31ff"],
+        borderWidth: 1,
+        borderColor: "#777",
+        hoverBorderWidth: 3,
+        hoverBorderColor: "#000",
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: true,
+    plugins: {
+      title: {
+        display: false,
+      },
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: true,
+      },
+    },
+    layout: {
+      padding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+      },
+    },
+  },
+});
